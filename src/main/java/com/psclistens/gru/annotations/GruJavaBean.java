13819cc59302370f0c6efff3d776ba4fd2218b41
Mike De Haan <mdehaan@psclistens.com>
2013-07-10 09:22:59 -0500

    Added Target to annotation

--- src/main/java/com/psclistens/gru/annotations/GruJavaBean.java
+++ src/main/java/com/psclistens/gru/annotations/GruJavaBean.java
@@ -1,13 +1,16 @@
 package com.psclistens.gru.annotations;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * User: mdehaan
  * Date: 7/1/13
  */
 @Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.TYPE)
 public @interface GruJavaBean {
 
 }
